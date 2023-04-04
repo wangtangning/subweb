5935e051fc363c2a12bda057af86e6a58a739491
Percy <percy@railway.app>
2023-04-04 10:09:35 +0000

    Initial Commit

---
+++ conf/config.js
@@ -0,0 +1,17 @@
+window.config = {
+  siteName: 'Subconverter Web',
+  apiUrl: 'http://127.0.0.1:25500',
+  shortUrl: 'https://s.ops.ci',
+  menuItem: [
+    {
+      title: '首页',
+      link: '/',
+      target: '',
+    },
+    {
+      title: 'GitHub',
+      link: 'https://github.com/stilleshan/subweb',
+      target: '_blank',
+    },
+  ],
+};
